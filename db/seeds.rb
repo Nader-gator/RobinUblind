8d15bc4736861958545798426caaa5c84300b579
Nader_Arbabian <Nader.arb72@gmail.com>
2019-01-23 18:28:15 -0500

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+User.destroy_all
 demo_user = User.create!(email: "demouser@demo.com", password: "starwars")

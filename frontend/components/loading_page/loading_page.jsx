e8e38c24551d081ad3d8eba0e7f6ad68e7f80af5
Nader_Arbabian <Nader.arb72@gmail.com>
2019-01-27 12:58:14 -0500

    style loading page, add file structure for charts

--- frontend/components/loading_page/loading_page.jsx
+++ frontend/components/loading_page/loading_page.jsx
@@ -4,11 +4,16 @@ import React from 'react'
 
 export default () => {
   return (
-    <div className="loading-page">
-      <h1>LOADING
+    <div className="entire-main-body loading-page">
 
-        if youre stuck here it mean API doesnt have stock information you clicked on, just hit back on your browser
-      </h1>
+    {/* <script>
+      $(function() {
+        $(".helper-text").delay(500).fadeIn()
+        });
+    </script> */}
+
+      <h1>LOADING</h1>
+      <p className="helper-text">If you're stuck Here that means the API doesn't have the stock you clicked on. just hit back on the browser</p>
     </div>
   )
 }
--- frontend/components/stock_show/stock_show.jsx
+++ frontend/components/stock_show/stock_show.jsx
@@ -2,106 +2,135 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Header from "../header/header_container"
 import Loading from '../loading_page/loading_page'
+import Chart from '../chart/chart_container'
+
+class ShowAndBuyForm extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state = {numShares: 0}
+  }
+
+  update(field){
+    return (e) => {
+      this.setState({[field]: e.target.value})
+    }
+  }
+
+  numberWithCommas(x){
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+  render(){
+    if (this.props.loading.newsLoading || this.props.loading.stockLoading) {
+      return <Loading />
+    }
 
 
-const StockShowPage = ({user, news, stock, loading }) => {
-
-  if (loading.newsLoading || loading.stockLoading) {
-    return <Loading/>
-  } 
-  return (
-    <div>
-      <header>
+    return (
+      <div>
+        <header>
           <Header />
 
           <script>
 
-          {document.addEventListener("DOMContentLoaded", () => {
-            window.onscroll = function () { myFunction() };
-            var navbar = document.getElementById("loggedin-navbar");
-            var sticky = navbar.offsetTop;
+            {document.addEventListener("DOMContentLoaded", () => {
+              window.onscroll = function () { myFunction() };
+              var navbar = document.getElementById("loggedin-navbar");
+              var sticky = navbar.offsetTop;
 
-            function myFunction() {
-              if (window.pageYOffset >= sticky) {
-                navbar.classList.add("sticky")
-              } else {
-                navbar.classList.remove("sticky");
+              function myFunction() {
+                if (window.pageYOffset >= sticky) {
+                  navbar.classList.add("sticky")
+                } else {
+                  navbar.classList.remove("sticky");
+                }
               }
-            }
-          })}
+            })}
 
-        </script>
-        
+          </script>
+
           <script>
 
-          {document.addEventListener("DOMContentLoaded", () => {
-            window.onscroll = function () { myFunction2() };
-            var navbar = document.getElementById("stock-buy");
-            var sticky = navbar.offsetTop;
+            {document.addEventListener("DOMContentLoaded", () => {
+              window.onscroll = function () { myFunction2() };
+              var navbar = document.getElementById("stock-buy");
+              var sticky = navbar.offsetTop;
 
-            function myFunction2() {
-              if (window.pageYOffset >= sticky) {
-                navbar.classList.add("bsticky")
-              } else {
-                navbar.classList.remove("bsticky");
+              function myFunction2() {
+                if (window.pageYOffset >= sticky) {
+                  navbar.classList.add("bsticky")
+                } else {
+                  navbar.classList.remove("bsticky");
+                }
               }
-            }
-          })}
+            })}
 
-        </script>
-
-
-      </header>
-      <div className="entire-main-body">
+          </script>
 
 
-
-        <div className="porfolio-performance">
-          <h1>$0.00</h1>
-          <h2>$0.00 (0.00%)<span>Today</span></h2>
-        </div>
-
-        <div className="porfolio-chart">
-          <h1>Chart goes here</h1>
-        </div>
-        {/* 22 Articles in here */}
-        <div className="news-list">
-          <h1>Recent News</h1>
-          <ul>
-            {news}
-          </ul>
-
-        </div>
+        </header>
+        <div className="entire-main-body">
 
 
 
-        <div id='stock-buy' className=" bsticky stock-buy">
-          <h2>
-            Buy {stock.quote.symbol}
-          </h2>
-          <span></span>
-          <div className="bottom-container">
-            <div className="share-selector">
-            <p>Shares</p>
+          <div className="porfolio-performance">
+            <h1>$0.00</h1>
+            <h2>$0.00 (0.00%)<span>Today</span></h2>
+          </div>
+
+          <div className="porfolio-chart">
+            {/* ---------------------------CHART GOES HERE----------------------------------- */}
+            <Chart test="Hi" />
+          </div>
+          {/* 22 Articles in here */}
+          <div className="news-list">
+            <h1>Recent News</h1>
+            <ul>
+              {this.props.news}
+            </ul>
+
+          </div>
+
+
+
+          <div id='stock-buy' className=" bsticky stock-buy">
+            <h2>
+              Buy {this.props.stock.quote.symbol}
+            </h2>
+            <span></span>
+            <div className="bottom-container">
+              <div className="share-selector">
+                <p>Shares</p>
+              </div>
+              <div className="market-price">
+                <p>Market Price</p>
+              </div>
+              <span className="divider"></span>
+
+              <div className="estimated-cost">
+                <p>Estimated Cost</p>
+              </div>
+              {/* new line here */}
+              <form >
+                <input className="number-shares-input" onChange={this.update("numShares")} type="number" />
+                <input className="buy-button" type="submit" value="submit-order" value="Submit Order" />
+              </form>
+              <span className="divider"></span>
+              <div className="buyingPower">${this.props.user.bankroll} is Available for Trading</div>
+              <div className="markey-price">${this.props.stock.quote.delayedPrice}</div>
+              <div className="estimated-cost-calc">${this.numberWithCommas(parseInt(this.props.stock.quote.delayedPrice) * this.state.numShares)}</div>
             </div>
-            <div className="market-price">
-              <p>Market Price</p>
-            </div>
-            <div className="estimated-cost">
-              <p>Estimated Cost</p>
-            </div>
-            <button className="buy-button" type="submit" value="submit-order">Submit Order</button>
-            <span className="divider"></span>
-            <div className="buyingPower">${user.bankroll} is Available for Trading</div>
-            {/* new line here */}
-            <input className="number-shares-input" type="text" onClick={()=>{return 0}} />
-            <div className="markey-price">{stock.quote.delayedPrice}</div>
+          </div>
         </div>
-        </div>
+
       </div>
+    )
+  }
 
-    </div>
-  )
 }
 
-export default StockShowPage
+
+
+
+export default ShowAndBuyForm

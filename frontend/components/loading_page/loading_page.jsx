a7c8030246df14981c563865e9557e90b84e8028
Nader_Arbabian <Nader.arb72@gmail.com>
2019-01-26 22:52:56 -0500

    add message to loading screen

--- frontend/components/loading_page/loading_page.jsx
+++ frontend/components/loading_page/loading_page.jsx
@@ -5,7 +5,10 @@ import React from 'react'
 export default () => {
   return (
     <div className="loading-page">
-      <h1>LOADING</h1>
+      <h1>LOADING
+
+        if youre stuck here it mean API doesnt have stock information you clicked on, just hit back on your browser
+      </h1>
     </div>
   )
 }

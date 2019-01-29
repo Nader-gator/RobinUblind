56f0eac3842227ce8bf2ec4618a3b8b489e0bcce
Nader_Arbabian <Nader.arb72@gmail.com>
2019-01-29 13:32:54 -0500

    remover debugger

--- app/controllers/api/transactions_controller.rb
+++ app/controllers/api/transactions_controller.rb
@@ -2,7 +2,7 @@ class Api::TransactionsController < ApplicationController
   def show
     user = User.find_by(id: params[:user_id])
 
-    debugger
+    
     if params[:date]
       transactions = user.sorted_transactions_upto(params[:date])
     else 
